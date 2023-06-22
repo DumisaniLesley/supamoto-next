c8f4912d2e365fe02742e904460f2e13233205b3
Supamoto-it <44291482+DumisaniLesley@users.noreply.github.com>
2023-06-22 13:13:15 +0200

    some spacing all over

--- app/zm/components/Companies.jsx
+++ app/zm/components/Companies.jsx
@@ -3,7 +3,7 @@ import data from '../data/companies.json'
 
 const Companies = () => {
   return (
-    <div className="max-w-6xl px-2 mx-auto mt-20">
+    <div className="max-w-6xl px-6 mx-auto mt-20">
         <div className="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-6">
             {data.companies.map((company)=>(
             <div key={company.id} className="flex items-center justify-center col-span-1 md:col-span-2 lg:col-span-1 bg-gray-100 py-5 rounded-lg shadow-xl cursor-pointer">
