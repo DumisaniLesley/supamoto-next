2bb860c3d374e8a471c0e363ddd5f90c965c07dd
Supamoto-it <44291482+DumisaniLesley@users.noreply.github.com>
2023-06-22 11:01:44 +0200

    changes

--- app/zm/about/page.js
+++ app/zm/about/page.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Hero from './Hero'
-// import Team from './team'
 import Features from './Features'
 import Brands from './Brands'
 import Socials from './Socials'
@@ -11,7 +10,6 @@ const page = () => {
       <Hero />
       <Features />
       <Brands />
-      {/* <Team /> */}
       <Socials />
     </>
   )
