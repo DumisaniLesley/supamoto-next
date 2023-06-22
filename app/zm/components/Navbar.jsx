19ee3eaa6e5cb8f7176290b518d0624c3db23de8
Supamoto-it <44291482+DumisaniLesley@users.noreply.github.com>
2023-06-22 13:09:18 +0200

    added mobile nav to "/zm"

--- app/zm/components/Navbar.jsx
+++ app/zm/components/Navbar.jsx
@@ -1,8 +1,19 @@
-import Link from 'next/link'
-import Logo from '@/public/supamoto.png'
-import Image from 'next/image'
+"use client";
+import { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
-const links = [
+import Logo from '@/public/supamoto.png';
+import { BiMenuAltRight } from 'react-icons/bi'
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const links = [
     {
         id: 1,
         title: 'Home',
@@ -35,27 +46,41 @@ const links = [
     }
 ]
 
-const Navbar = () => {
   return (
-    <nav  className="fixed w-full bg-white shadow-xl z-50">
-    <div className="container max-w-6xl px-2 py-5 mx-auto md:flex md:justify-between md:items-center">
+    <nav className="fixed w-full bg-white shadow-xl z-50">
+      <div className="container max-w-6xl px-2 py-5 mx-auto md:flex md:justify-between md:items-center">
         <div className="flex items-center justify-between">
-            <a href="/">
-                <Image width={100} height={24} src={Logo} alt=""/>
-            </a>
+          <Link href="/">
+            <Image width={100} height={24} src={Logo} alt="" />
+          </Link>
+          <button
+            type="button"
+            className="block md:hidden"
+            onClick={toggleNavbar}
+          >
+            <BiMenuAltRight className="w-12 h-12 fill-current text-gray-900" />
+          </button>
         </div>
 
         {/* <!-- Mobile Menu open: "block", Menu closed: "hidden" --> */}
-        <div class="absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out md:mt-0 md:p-0 md:top-0 md:relative md:bg-transparent md:w-auto md:opacity-100 md:translate-x-0 md:flex md:items-center">
-            <div class="flex flex-col md:flex-row md:mx-6">
-                {links.map((link) => (
-                    <Link href={link.url} key={link.id} className="my-2 text-md font-semibold transition-colors duration-300 transform  text-orange-600 hover:text-blue-500 md:mx-4 md:my-0">{link.title}</Link>
-                    ))}
-            </div>
+        <div
+          className={`${
+            isOpen ? 'block' : 'hidden'
+          } mt-5 absolute inset-x-0 z-20 w-full h-screen text-center backdrop-blur backdrop-opacity-65 text-lg font-semibold px-6 py-8 transition-all duration-300 ease-in-out md:mt-0 md:p-0 md:top-0 md:relative md:bg-transparent md:w-auto md:opacity-100 md:translate-x-0 md:flex md:items-center`}
+        >
+          <div className="flex flex-col md:flex-row md:mx-6">
+            {links.map((link) => (
+              <Link 
+                href={link.url} 
+                key={link.id} 
+                onClick={toggleNavbar} 
+                className="my-2 text-lg font-semibold text-orange-600 transition-colors duration-300 transform  hover:text-blue-600 md:mx-4 md:my-0">{link.title}</Link>
+            ))}
+          </div>
         </div>
-    </div>
-</nav>
-  )
-}
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
